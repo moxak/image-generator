e975114461a099ce1046e56dfbe091fb26e8b7dc
moxak <smileyjoies@email.com>
2023-03-18 14:04:36 +0900

    impl: passing any color to gradient image

--- scripts/gradient.go
+++ scripts/gradient.go
@@ -13,52 +13,49 @@ import (
     "golang.org/x/image/draw"
 )
 
-
-func drawGradient(width, height int, angle float64, startColor, midColor, endColor color.RGBA) *image.RGBA {
+func drawGradient(width, height int, angle float64, colors ...color.RGBA) *image.RGBA {
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
-	draw.Draw(img, img.Bounds(), &image.Uniform{startColor}, image.ZP, draw.Src)
+	draw.Draw(img, img.Bounds(), &image.Uniform{colors[0]}, image.ZP, draw.Src)
 
-	dx := float64(width) * math.Cos(angle*math.Pi/180)
-	dy := float64(height) * math.Sin(angle*math.Pi/180)
+    // Calculate the direction of the gradient
+	dx := float64(width) * math.Sin(angle*math.Pi/180)
+	dy := float64(height) * math.Cos(angle*math.Pi/180)
 
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			// calculate linear interpolation between startColor, midColor and endColor
-			// based on position in the gradient
-			gradientPos := ((float64(x)*dx + float64(y)*dy) / (dx*dx + dy*dy))
-			var r, g, b, a uint8
-			if gradientPos < 0.5 {
-				pos := gradientPos * 2
-				r = uint8(float64(startColor.R)*(1-pos) + float64(midColor.R)*pos)
-				g = uint8(float64(startColor.G)*(1-pos) + float64(midColor.G)*pos)
-				b = uint8(float64(startColor.B)*(1-pos) + float64(midColor.B)*pos)
-				a = uint8(float64(startColor.A)*(1-pos) + float64(midColor.A)*pos)
-			} else {
-				pos := (gradientPos - 0.5) * 2
-				r = uint8(float64(midColor.R)*(1-pos) + float64(endColor.R)*pos)
-				g = uint8(float64(midColor.G)*(1-pos) + float64(endColor.G)*pos)
-				b = uint8(float64(midColor.B)*(1-pos) + float64(endColor.B)*pos)
-				a = uint8(float64(midColor.A)*(1-pos) + float64(endColor.A)*pos)
-			}
+	// Define the number of segments based on the number of colors
+	numSegments := len(colors) - 1
+	for i := 0; i < width; i++ {
+		for j := 0; j < height; j++ {
+			// Calculate the position of the current pixel along the gradient
+			gradientPos := (float64(i)*dx + float64(j)*dy) / (float64(width)*dx + float64(height)*dy)
 
-			img.Set(x, y, color.RGBA{r, g, b, a})
+			// Find the two colors that this pixel lies between
+			segment := int(gradientPos * float64(numSegments))
+			startColor := colors[segment]
+			endColor := colors[segment+1]
+
+			// Calculate the color of the current pixel by interpolating between the two colors
+			segmentPos := gradientPos*float64(numSegments) - float64(segment)
+			r := uint8(float64(startColor.R)*(1-segmentPos) + float64(endColor.R)*segmentPos)
+			g := uint8(float64(startColor.G)*(1-segmentPos) + float64(endColor.G)*segmentPos)
+			b := uint8(float64(startColor.B)*(1-segmentPos) + float64(endColor.B)*segmentPos)
+			a := uint8(float64(startColor.A)*(1-segmentPos) + float64(endColor.A)*segmentPos)
+			img.Set(i, j, color.RGBA{r, g, b, a})
 		}
 	}
 	return img
 }
 
 
-
 func main() {
     // Parse arguments
-    width := flag.Int("size", 256, "Width of the image")
+    width := flag.Int("w", 256, "Width of the image")
 	height := flag.Int("h", 256, "Height of the image")
     angle := flag.Float64("angle", 0.0, "Angle of rotation in degrees")
     output := flag.String("o", "output.jpg", "Output file name")
     flag.Parse()
 
     // Draw gradient
-    img := drawGradient(*width, *height, *angle, color.RGBA{254,190,90, 255}, color.RGBA{222,50,76, 255},color.RGBA{140,67,174,255})
+    img := drawGradient(*width, *height, *angle, color.RGBA{254,190,90, 255}, color.RGBA{222,50,76, 255}, color.RGBA{193, 52,104, 255}, color.RGBA{140,67,174,255})
 
     // Save image to file
 	file, err := os.Create(*output)
