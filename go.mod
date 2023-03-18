713e92c7eb6e977b9e4610f714459ca4e83d1586
moxak <smileyjoies@email.com>
2023-03-18 13:16:17 +0900

    impl: gradient image generator with angle specified

---
+++ go.mod
@@ -0,0 +1,7 @@
+module moxak.com/image-generator
+
+go 1.18
+
+require (
+	golang.org/x/image v0.6.0 // indirect
+)
--- scripts/gradient.go
+++ scripts/gradient.go
@@ -1,42 +1,63 @@
 package main
 
 import (
-	"image"
-	"image/color"
-	"image/jpeg"
-	"log"
-	"os"
+    "fmt"
+    "flag"
+    "image"
+    "image/color"
+    "image/png"
+    "os"
+    "log"
+    "math"
+
+    "golang.org/x/image/draw"
 )
 
-func main() {
-	// 画像サイズを設定
-	width := 256
-	height := 256
-
-	// 画像を生成
+func drawGradient(width, height int, angle float64, startColor, endColor color.RGBA) *image.RGBA {
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	draw.Draw(img, img.Bounds(), &image.Uniform{startColor}, image.ZP, draw.Src)
 
-	// グラデーションの色を設定
-	color1 := color.RGBA{255, 0, 0, 255} // 赤
-	color2 := color.RGBA{0, 0, 255, 255} // 青
+	dx := float64(width) * math.Cos(angle*math.Pi/180)
+	dy := float64(height) * math.Sin(angle*math.Pi/180)
 
-	// グラデーションを生成
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			ratio := float64(x) / float64(width)
-			r := uint8(float64(color1.R)*(1-ratio) + float64(color2.R)*ratio)
-			g := uint8(float64(color1.G)*(1-ratio) + float64(color2.G)*ratio)
-			b := uint8(float64(color1.B)*(1-ratio) + float64(color2.B)*ratio)
-			img.Set(x, y, color.RGBA{r, g, b, 255})
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			// calculate linear interpolation between startColor and endColor
+			// based on position in the gradient
+			gradientPos := ((float64(x)*dx + float64(y)*dy) / (dx*dx + dy*dy))
+			r := uint8(float64(startColor.R)*(1-gradientPos) + float64(endColor.R)*gradientPos)
+			g := uint8(float64(startColor.G)*(1-gradientPos) + float64(endColor.G)*gradientPos)
+			b := uint8(float64(startColor.B)*(1-gradientPos) + float64(endColor.B)*gradientPos)
+			a := uint8(float64(startColor.A)*(1-gradientPos) + float64(endColor.A)*gradientPos)
+
+			img.Set(x, y, color.RGBA{r, g, b, a})
 		}
 	}
+	return img
+}
 
-	// 画像をJPEG形式で保存
-	file, err := os.Create("output/gradient.jpg")
+func main() {
+    // Parse arguments
+    width := flag.Int("size", 256, "Width of the image")
+	height := flag.Int("h", 256, "Height of the image")
+    angle := flag.Float64("angle", 0.0, "Angle of rotation in degrees")
+    output := flag.String("o", "output.jpg", "Output file name")
+    flag.Parse()
+
+    // Draw gradient
+    img := drawGradient(*width, *height, *angle, color.RGBA{254,190,90, 255}, color.RGBA{140,67,174,255})
+
+    // Save image to file
+	file, err := os.Create(*output)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	jpeg.Encode(file, img, &jpeg.Options{100})
+	err = png.Encode(file, img)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+    fmt.Printf("Image saved to %s\n", *output)
 }
